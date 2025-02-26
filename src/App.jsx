4ae6530f0d17bd3960803a298eb2c6535ef633c2
Jitendra Bhati <Anuragbhati679@gmail.com>
2025-02-26 10:21:20 +0530

    Final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUser } from '@clerk/clerk-react'
 import Header from './components/custom/Header'
@@ -10,17 +7,16 @@ import { Toaster } from './components/ui/sonner'
 
 function App() {
   const [count, setCount] = useState(0)
-  const {user,isLoaded,isSignedIn}=useUser();
+  const { user, isLoaded, isSignedIn } = useUser();
 
-  if(!isSignedIn&&isLoaded)
-  {
+  if (!isSignedIn && isLoaded) {
     return <Navigate to={'/auth/sign-in'} />
   }
 
   return (
     <>
-      <Header/>
-      <Outlet/>
+      <Header />
+      <Outlet />
       <Toaster />
     </>
   )
